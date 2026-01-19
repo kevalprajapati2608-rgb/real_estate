dc251b3af2b7d440a42d445a756125332343d132
keval <kevalp028.gmail.com.>
2026-01-19 14:14:17 +0530

    sign up page

--- api/index.js
+++ api/index.js
@@ -1,18 +1,36 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log("Connected to MongoDB")
-}).catch((err) => {
-   console.log("Error :"+err); 
+const app = express();
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error :" + err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 !!!");
 });
 
-
-
-
-const app  = express();
-app.listen(3000, () => {
-    console.log("Server is running on port 3000 !!!");
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Sever Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
